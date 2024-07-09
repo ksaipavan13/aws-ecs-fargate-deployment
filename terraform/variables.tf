56024feb6bad16bd6d263abe111a9ed93b8b56be
saipavan <ksp2001961@gmail.com>
2024-07-09 15:53:38 +0530

    Initial commit with ReactJS and Terraform files

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_account_id" {
+  description = "The AWS account ID"
+  default     = "533266978173"
+}
+
+variable "region" {
+  description = "The AWS region"
+  default     = "ca-central-1"
+}
+
